d2485ad91e701aaf2e5537e8e5a87bf277b2ca8e
unknown <christopherortega56@gmail.com>
2020-02-10 17:39:43 -0600

    add text

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello')
+import PyPDF2
+import sys
+
+inputs = sys.argv[1:]
+
+
+def pdf_combiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        merger.append(pdf)
+    merger.write('combined.pdf')
+
+
+pdf_combiner(inputs)
